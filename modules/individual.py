e5743bf88323ca2451088e01e84e2255fd7123fc
Rafael Fernandes de Almeida Gomes <rafaelfagomes@Rafaels-MacBook-Air-2.local>
2023-10-09 16:43:59 +0100

    implemented individual data type

--- modules/individual.py
+++ modules/individual.py
@@ -3,6 +3,25 @@
 
 
 # Implementação:
-#   Data:
-#
-#
+#   Data: list[float, colouring]
+#   O float é o tempo em que o Individuo "nasceu"
+#   O Any é qualquer objeto, de forma a que a cada individuo fique associado um objeto desse tipo
+
+
+Individual = tuple[float, type]
+
+
+def new_individual(age: float, core: type) -> Individual:
+    return (age, core)
+
+
+def age(current_time: float, individual: Individual) -> float:
+    return current_time - individual[0]
+
+
+def get_core(individual: Individual) -> type:
+    return individual[1]
+
+
+def update_core(individual: Individual, new_core: type) -> Individual:
+    return (individual[0], new_core)
