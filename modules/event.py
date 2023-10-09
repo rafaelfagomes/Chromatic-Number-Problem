31a5a1496603913f3bc26448e3483da78b3c9fc3
Rafael Fernandes de Almeida Gomes <rafaelfagomes@Rafaels-MacBook-Air-2.local>
2023-10-09 16:00:25 +0100

    implemented event data type

--- modules/event.py
+++ modules/event.py
@@ -3,6 +3,24 @@
 
 
 # Implementação:
-#   Data:
-#
-#
+#   Data: tuple[int,str,int]
+#   O float representa o tempo em que o evento deve ser realizado, a str representa o tipo do evento
+#   O int representa o identificador do individuo associado ao evento
+
+Event = tuple[float, str, int]
+
+
+def new_event(time: float, kind: str, object_identifier: int) -> Event:
+    return (time, kind, object_identifier)
+
+
+def time(event: Event) -> float:
+    return event[0]
+
+
+def kind(event: Event) -> str:
+    return event[1]
+
+
+def object_identifier(event: Event) -> int:
+    return event[2]
